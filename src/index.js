c6a34e10e383269357290bef30495be809a8a238
Niko Musaev <nikitoz9999@gmail..com>
2019-07-02 22:39:42 +0500

    Solving a task with validation of brackets (https://github.com/SIARHEI-SMANTSAR/new-brackets)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    var stack = [];
+    var opensBrackets = [];
+    var closedBrackets = [];
+
+    bracketsConfig.forEach(function (item, index) {
+        opensBrackets.push(item[0]);
+        closedBrackets.push(item[1]);
+        return;
+    });
+
+    for (var i = 0; i < str.length; i++) {
+        if (stack.length == 0) {
+            stack.push(str.charAt(i));
+            if (opensBrackets.indexOf(str.charAt(i)) != -1) {
+                continue;
+            } else {
+                break;
+            }
+        }
+        if (opensBrackets.indexOf(stack[stack.length - 1]) == closedBrackets.indexOf(str.charAt(i))) {
+            stack.pop();
+            continue;
+        }
+        if (opensBrackets.indexOf(str.charAt(i)) != -1) {
+            stack.push(str.charAt(i));
+            continue;
+        } else {
+            break;
+        }
+    }
+
+    return stack.length == 0;
+
 }
